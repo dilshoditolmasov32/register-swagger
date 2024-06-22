7907d6a0e324e62c16f7badb576151f27ad07275
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2024-06-22 09:02:32 +0500

    register page created

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,7 @@
 //   tailwindcss: {},
 //   autoprefixer: {},
 // };
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+export const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+};
